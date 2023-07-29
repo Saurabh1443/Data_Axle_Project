e15f6203c9f58f23a1cf50c73609d48c49e8aca3
Shruti Sahu <52shrutisahu@gmail.com>
2023-07-30 00:45:20 +0530

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
-  return <div className="App">Welcome</div>;
+  return <Login />;
 }
 
 export default App;

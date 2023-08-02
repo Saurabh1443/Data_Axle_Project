49a13ad42e1db1636a7967fba78578460d9c0875
Ayush Bhagat <bhagatayush8818@gmail.com>
2023-08-02 11:57:03 +0530

    Added drawer

---
+++ Backend/emailGeneration/EmailGeneratorManager.py
@@ -0,0 +1,50 @@
+import openai
+from dotenv import load_dotenv
+load_dotenv() 
+import os
+openai.api_key  = str(os.getenv('OPEN_AI_API_KEY'))
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0.5, 
+    )
+    return response.choices[0].message['content']
+
+def remove_null_or_empty_values(dictionary):
+    return {key: value for key, value in dictionary.items() if value is not None and value != ""}
+
+def responseGenerator(personal_information,Attributes):
+    personal_information = remove_null_or_empty_values(personal_information)
+
+    prompt = f"""your objective is to compose a concise, personalized \
+email with subject using the provided information. 
+Before generating email,
+    i. first check product description under Attributes and analyze product description.\
+    ii. Given a product description in one word, check if it corresponds to an existing product or brand name.\
+    iii. Given a product description in sentences, verify that the description pertains to a specific product and does not contain any irrelevant information beyond the product description.\
+    iv. If any of ii or iii condition is not satisfying then don't follow below guidelines and just print "Please, enter a valid product description."\
+
+Follow the guidelines below:\
+1. Generate a short email with maximum 2 lines in each paragraph with maximum 3 paragraphs.
+2. Use the "Attributes" and "Personal Information" delimited in triple backticks for generating dynamic content in email.\
+3. Attributes section is provided by Product owner and Personal information is of a target person.\
+4. Compose a compelling email that aligns with the given Product Description, email tone and Email tone description under attributes.\
+5. Feel free to use emojis or other Unicode characters to add emphasis or express emotions in the email.\
+6. Strictly, Do not include product descriptions, features, or any special offers, deals, or discounts in the email, unless explicitly mentioned in the Product Description.\
+7. Utilize as many fields under "Personal Information" as possible in combination to create an engaging \
+email.\
+8. If you come across any irrelevant fields under "Personal Information", unrelated to the product \
+or unsuitable for the email, please omit them. Prioritize the recipient's name \
+and relevant details to ensure a meaningful email. \
+9. Remember you are prohibited from including PII data fields present in Personal Information under Attributes in email \
+and focus on engaging the recipient with a personalized message. \
+  
+Attributes:```{Attributes}```\
+
+Personal Information:```{personal_information}```\
+
+"""
+    return get_completion(prompt)

dcf683efffda5e586b3198701bd4473e5de0fc73
Saurabh1443 <rjnsaurabh143@gmail.com>
2023-08-22 12:19:51 +0530

    Added templates to show all mails

--- frontend/src/components/AllMails/Mails.jsx
+++ frontend/src/components/AllMails/Mails.jsx
@@ -1,39 +1,28 @@
 import React, { useState, useEffect } from "react";
 import Paper from "@mui/material/Paper";
-import { DataGrid } from "@mui/x-data-grid";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import _ from "lodash";
 import { MuiNavbar } from "../Navbar/navbar";
 import Footer from "../Dashboard/Footer";
-import {
-  Card,
-  CardActionArea,
-  CardContent,
-  CardHeader,
-  Grid,
-} from "@mui/material";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from '@mui/material/Divider';
+import ListItemText from "@mui/material/ListItemText";
+import ListItemButton from '@mui/material/ListItemButton';
+import Grid from '@mui/material/Grid';
 
 export default function AllMails() {
   const [rows, setRows] = useState([]);
   const [gridLoading, setGridLoading] = useState(false);
   const [totalEntries, setTotalEntries] = useState("");
+  const [emailDetails, setEmailDetails] = useState({})
+  const [selectedIndex ,setSelectedIndex] = useState(-1)
 
-  const columns = [
-    { field: "receiverEmail", headerName: "Receiver Mail", width: 270 },
-    { field: "subject", headerName: "Subject", width: 300 },
-    { field: "message", headerName: "Message", width: 520 },
-    {
-      field: "sendAt",
-      headerName: "Sent At",
-      width: "max-content",
-      renderCell: (vv) => {
-        return <>{vv?.formattedValue?.split("T")?.[0]}</>;
-      },
-    },
-  ];
+
+  const handleEmailClick = (vv,id) => {
+    setSelectedIndex(id);
+    setEmailDetails(vv)
+  }
+  
 
   useEffect(() => {
     setGridLoading(true);
@@ -42,7 +31,7 @@ export default function AllMails() {
         const apiUrl = `http://127.0.0.1:8000/api/persons/getMail`;
         const response = await fetch(apiUrl);
         const { result, error, success } = await response.json();
-
+         
         setTotalEntries(result?.length);
         setRows(result);
       } catch (error) {
@@ -54,27 +43,32 @@ export default function AllMails() {
   }, []);
 
   return (
+    
     <Paper sx={{ width: "100%", overflow: "hidden" }}>
       <MuiNavbar />
-      <Grid
-        container
-        spacing={3}
-        sx={{
-          mt: 10,
-        }}
-      >
-        {rows?.map((vv, index) => {
-          return (
-            <Grid key={index} item xs={3}>
-              <Card sx={{ maxWidth: "75%", ml: 3 }}>
-                <CardHeader subheader={vv?.receiverEmail}></CardHeader>
-                <CardContent></CardContent>
-              </Card>
-            </Grid>
-          );
-        })}
-      </Grid>
-
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+        <Paper>
+      <List sx={{ mt: 10 ,ml:4 }}>
+        {rows?.map((vv) => (
+          <>
+            <ListItem key={vv?.id} disablePadding>
+              <ListItemButton selected={selectedIndex ===vv?.id} onClick={() => handleEmailClick(vv, vv?.id)} >
+                <ListItemText primary={vv?.receiverEmail} secondary={vv?.subject} />
+                </ListItemButton>
+            </ListItem>
+            <Divider />
+          </>
+        ))}
+          </List>
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper >
+            {selectedIndex ? (<><box><h1>{emailDetails?.subject}</h1></box></>):(<></>)}
+          </Paper>
+        </Grid>
+        </Grid>
       <Footer />
     </Paper>
   );
